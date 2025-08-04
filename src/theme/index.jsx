5143fd3c592a50e4de4b6434cd4e5b0efb85396a
Sonia CHELLABI <chellabisonia@gmail.com>
2025-08-04 18:46:27 +0200

    F01: Add Font Family and Theme

---
+++ src/theme/index.jsx
@@ -0,0 +1,55 @@
+    const colors ={
+        nude:"#d6bca5",
+        inputDark: "#383735",
+        backgroundDark:"#141414",
+        white:"#ffffff",
+        input:  "#d3dbd0",
+    };
+
+    const spacing ={
+        xxs: "4px",
+        xs: "8px",
+        sm: "12px",
+        md: "20px",
+        lg: "32px",
+        xl: "52px",
+        xxl: "84px",
+    };
+
+    const fonts = {
+        sizes: {
+            XXXS: "8px",
+            XXS: "10px",
+            XS: "12px",
+            SM: "15px",
+            P0: "16px",
+            P1: "18px",
+            P2: "20px",
+            P3: "24px",
+            P4: "28px",
+            P5: "48px",
+            P6: "60px",
+        },
+
+        weights: {
+            light: 300,
+            regular: 400,
+            medium: 500,
+            semiBold: 600,
+            bold: 700,
+            heavy: 800,
+        },
+    };
+    const borderRadius = {
+        subtle: 1,
+        round: "5px",
+        extraRound: "13px",
+        circle: "50%",
+    };
+
+    export const theme = {
+        colors,
+        spacing,
+        fonts,
+        borderRadius,
+    }
