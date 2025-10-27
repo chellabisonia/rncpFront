a1ff9c781334ea1618dd6fe1915a063cfa845df4
Sonia CHELLABI <chellabisonia@gmail.com>
2025-10-27 12:29:48 +0100

    :recycle: refactor: cleaned up file structure and updated styles to match new design system

--- src/components/reusable-ui/TextInput.jsx
+++ src/components/reusable-ui/TextInput.jsx
@@ -3,58 +3,32 @@ import styled from "styled-components";
 import TextField from '@mui/material/TextField';
 import { theme } from "../../theme/index.jsx";
 
-export default function TextInput({ variant = "outlined", value, label, onChange, type, placeholder, }) {
+export default function TextInput({ variant = "outlined", value, label, onChange, type, placeholder,name }) {
     return (
         <StyledTextField
-            variant={variant}
-            placeholder={placeholder}
+            name={name}
             label={label}
-            value={value}
+            value={value ?? ""}
             onChange={onChange}
             type={type}
-            InputLabelProps={{
-                shrink: true // label toujours au-dessus
-            }}
-            fullWidth //  s'adapte automatiquement à la largeur du parent
-            InputProps={{
-                disableUnderline: true // enlève la ligne MUI par défaut
-            }}
+            placeholder={placeholder}
+            variant={variant}
+            fullWidth
             autoComplete="off"
+            InputLabelProps={{ shrink: true }}
+            InputProps={{ disableUnderline: true }}
         />
     );
 }
-
-/*
 const StyledTextField = styled(TextField)`
-    background-color: ${theme.colors.input};
-    border-radius: ${theme.fonts.sizes.P3};
+    background-color: ${theme.colors.white};
     height: 42px;
-    /!* Garder même couleur au focus *!/
-    & .MuiFilledInput-root.Mui-focused {
-        background-color: ${theme.colors.input};
-    }
+    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.25);
 
-    /!* Supprimer soulignement bleu (filled) *!/
-    & .MuiFilledInput-underline:after {
-        border-bottom: 2px solid #d9ad23 !important;
+    & .MuiInputBase-input::placeholder {
+        color: ${theme.colors.inputDark} !important;  /* ✅ priorité accrue */
+        opacity: 0.8; /* sinon MUI réduit la visibilité du placeholder */
     }
-
-    /!* Label focus *!/
-    & .MuiInputLabel-root.Mui-focused {
-        color: ${theme.colors.inputDark};
-    }
-
-    /!* Coins arrondis *!/
-    & .MuiFilledInput-root {
-        border-radius: ${theme.fonts.sizes.P3};
-    }
-`;
-*/
-
-const StyledTextField = styled(TextField)`
-    background-color: ${theme.colors.input};
-    border-radius: ${theme.fonts.sizes.P3};
-    height: 42px;
 
     /* Applique la même hauteur au container et à l'input interne */
     & .MuiInputBase-root {
@@ -75,26 +49,18 @@ const StyledTextField = styled(TextField)`
         outline: none;
         box-shadow: none;
     }
- 
-
-    /* Bordure focus (variant outlined) */
-    & .MuiOutlinedInput-notchedOutline {
-        border-color: darkred;
-    }
-    & .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline {
-        border-color: red;
-    }
-
+    
     /* Label focus */
     & .MuiInputLabel-root.Mui-focused {
         color: ${theme.colors.inputDark};
     }
     & .MuiInputLabel-root.MuiInputLabel-shrink {
-        transform: translate(-4px, -15px) scale(0.85);
-        color: white;/* monte le label */
-        font-size: 14px;
-        font-family: ${theme.fonts.family};
+        transform: translate(-4px, -20px) scale(0.85);
+        color: ${theme.colors.inputDark};/* monte le label */
+        font-size: 16px;
+        font-family: ${theme.fonts.family.fontFamily};
         line-height: 20px;
+        font-weight: ${theme.fonts.weights.heavy};
     }
 `;
 
